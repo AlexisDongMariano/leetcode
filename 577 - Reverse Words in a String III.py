e16c0ae978f682f4d50f94d11c3acb5134623c52
unknown <alexisdongmariano@gmail.com>
2021-03-26 22:30:26 -0700

    modified py file

--- 577 - Reverse Words in a String III.py
+++ 577 - Reverse Words in a String III.py
@@ -2,60 +2,39 @@
 #         Information
 # ==============================
 
-# Title: 1 - Two Sum
-# Link: https://leetcode.com/problems/two-sum/description/
+# Title: 577 - Reverse Words in a String III
+# Link: https://leetcode.com/problems/reverse-words-in-a-string-iii/
 # Difficulty: Easy
 # Language: Python
 
 # Problem:
-# Given an array of integers nums and an integer target, return indices of the
-# two numbers such that they add up to target.
-# You may assume that each input would have exactly one solution, and you may
-# not use the same element twice.
-# You can return the answer in any order.
+# Given a string s, reverse the order of characters in each word within a
+# sentence while still preserving whitespace and initial word order.
 
 # Example
-# Input: nums = [2,7,11,15], target = 9
-# Output: [0,1]
-# Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+# Input: s = "Let's take LeetCode contest"
+# Output: "s'teL ekat edoCteeL tsetnoc"
 
 # ==============================
 #         Solution
 # ==============================
 
-# 0(n^2)
-def two_sum(nums, target):
-    length = len(nums)
+# 0(m * n)
+def reverse_words(s):
+    s_list = s.split(' ')
+    output = []
+    for s in s_list:
+        r = s[::-1]
+        print(r)
+        output.append(r)
+    return ' '.join(output)
 
-    for i in range(length - 1):
-        for j in range(i + 1, length):
-            if nums[i] + nums[j] == target:
-                return [i, j]
+
+# shortened version of above
+def reverse_words2(s):
+    return ' '.join([w[::-1] for w in s.split()])
 
 
-# 0(n)
-def two_sum2(nums, target):
-    if not nums:
-        return 'list is empty'
-    
-    if type(target) != int:
-        return 'target is not a valid integer'
-
-    values = {}
-
-    for idx, num in enumerate(nums):
-        diff = target - num
-
-        if num in values.keys():
-            return [values[num], idx]
-        else:
-            values[diff] = idx
-    
-    return 'no match'
-        
-
-nums = [2,7,11,15]
-target = 9
-
-print(two_sum(nums, target))
-print(two_sum2(nums, target))
+s = "Let's take LeetCode contest"
+print(reverse_words(s))
+print(reverse_words2(s))
