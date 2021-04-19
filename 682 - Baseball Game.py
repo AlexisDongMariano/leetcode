13c2b364f1344d336b24fe8212268ecd0e7f117f
unknown <alexisdongmariano@gmail.com>
2021-04-18 23:26:46 -0700

    added py file

---
+++ 682 - Baseball Game.py
@@ -0,0 +1,61 @@
+# ==============================
+#         Information
+# ==============================
+
+# Title: 682 - Baseball Game
+# Link: https://leetcode.com/problems/baseball-game/
+# Difficulty: Easy
+# Language: Python
+
+# Problem:
+# You are keeping score for a baseball game with strange rules. The game
+# consists of several rounds, where the scores of past rounds may affect
+# future rounds' scores.
+
+# At the beginning of the game, you start with an empty record. You are
+# given a list of strings ops, where ops[i] is the ith operation you must
+# apply to the record and is one of the following:
+
+# An integer x - Record a new score of x.
+# "+" - Record a new score that is the sum of the previous two scores. It is
+# guaranteed there will always be two previous scores.
+# "D" - Record a new score that is double the previous score. It is guaranteed
+# there will always be a previous score.
+# "C" - Invalidate the previous score, removing it from the record. It is
+# guaranteed there will always be a previous score.
+# Return the sum of all the scores on the record.
+
+# Example
+# Input: ops = ["5","2","C","D","+"]
+# Output: 30
+# Explanation:
+# "5" - Add 5 to the record, record is now [5].
+# "2" - Add 2 to the record, record is now [5, 2].
+# "C" - Invalidate and remove the previous score, record is now [5].
+# "D" - Add 2 * 5 = 10 to the record, record is now [5, 10].
+# "+" - Add 5 + 10 = 15 to the record, record is now [5, 10, 15].
+# The total sum is 5 + 10 + 15 = 30.
+
+# ==============================
+#         Solution
+# ==============================
+
+
+def calculate_points(ops):
+    points = []
+    for i in ops:
+        if i == 'C':
+            points.pop()
+        elif i == 'D':
+            points.append(points[-1] * 2)
+        elif i == '+':
+            points.append(points[-2] + points[-1])
+        else:
+            points.append(int(i))
+    return sum(points)
+
+
+ops = ["5","2","C","D","+"]
+ops2 = ["5","-2","4","C","D","9","+","+"]
+print(calculate_points(ops))
+print(calculate_points(ops2))
